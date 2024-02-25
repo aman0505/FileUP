7e7be97e1fbdbdf90e46709aacd5b4ce2547b91b
aman0505 <rauniyaraman463@gmail.com>
2024-02-25 23:46:41 +0530

    search[routes] fix file icon on download templte

--- src/components/searchfile/searchfile.svelte
+++ src/components/searchfile/searchfile.svelte
@@ -5,7 +5,8 @@
 <main>
 	<div class="flex gap-4 items-center border-1 border-solid border-gray-400">
 		<div class="w-[50px] h-[50px] shrink-0">
-			<img src="../../../static/file-zip-icon-6848.png" alt="" />
+			<!-- <img src="../../../static/file-zip-icon-6848.png" alt="" /> -->
+			<i class="fa-solid fa-file-zipper text-4xl text-yellow-600"></i>
 		</div>
 		<p>{responsedata?.FileName ? responsedata.FileName : responsedata.FileUnqueId}</p>
 		<a
